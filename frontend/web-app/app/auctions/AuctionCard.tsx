35e6e328b3cbc4151e304e2ecad4a4f48a9e4d0f
Tung Dao <daotung492@gmail.com>
2024-12-24 10:12:54 +0700

    update client side

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,24 +1,33 @@
-import Image from 'next/image';
+import React from 'react'
+import CountdownTimer from './CountdownTimer';
+import CarImage from './CarImage';
+import { Auction } from '@/types';
+import Link from 'next/link';
+import CurrentBid from './CurrentBid';
 
 type Props = {
-    auction: any;
+    auction: Auction;
 }
 
 export default function AuctionCard({ auction }: Props) {
     return (
-        <a href="#">
-            <div className="relative w-full bg-gray-200 aspect-video rounded-lg overflow-hidden">
-                <Image src={auction.imageUrl}
-                    alt={`Image of ${auction.make} ${auction.model} in ${auction.color}`}
-                    fill
-                    className='object-cover'
-                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw'
-                />
+        <Link href={`/auctions/details/${auction.id}`} className='group'>
+            <div className='relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden'>
+                <CarImage imageUrl={auction.imageUrl} />
+                <div className='absolute bottom-2 left-2'>
+                    <CountdownTimer auctionEnd={auction.auctionEnd} />
+                </div>
+                <div className='absolute top-2 right-2'>
+                    <CurrentBid 
+                        reservePrice={auction.reservePrice} 
+                        amount={auction.currentHighBid}/>
+                </div>
             </div>
             <div className='flex justify-between items-center mt-4'>
                 <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
-                <span className='text-sm font-semibold'>{auction.price}</span>
+                <p className='font-semibold text-sm'>{auction.year}</p>
             </div>
-        </a>
+
+        </Link>
     )
 }
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,18 +1,68 @@
-import AuctionCard from "./AuctionCard";
+'use client'
 
-async function getData() {
-    const res = await fetch('http://localhost:6001/search');
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
+import React, { useEffect, useState } from 'react'
+import AuctionCard from './AuctionCard';
+import AppPagination from '../conponents/AppPagination';
+import { Auction, PagedResult } from '@/types';
+import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { useShallow } from 'zustand/react/shallow';
+import qs from 'query-string';
+import EmptyFilter from '../conponents/EmptyFilter';
+import { useAuctionStore } from '@/hooks/useAuctionStore';
+
+export default function Listings() {
+    const [loading, setLoading] = useState(true);
+    const params = useParamsStore(useShallow(state => ({
+        pageNumber: state.pageNumber,
+        pageSize: state.pageSize,
+        searchTerm: state.searchTerm,
+        orderBy: state.orderBy,
+        filterBy: state.filterBy,
+        seller: state.seller,
+        winner: state.winner
+    })));
+    const data = useAuctionStore(useShallow(state => ({
+        auctions: state.auctions,
+        totalCount: state.totalCount,
+        pageCount: state.pageCount
+    })))
+    const setData = useAuctionStore(state => state.setData);
+    const setParams = useParamsStore(state => state.setParams);
+    const url = qs.stringifyUrl({ url: '', query: params });
+
+    function setPageNumber(pageNumber: number) {
+        setParams({ pageNumber })
     }
-    return res.json();
-}
 
-export default async function Listings() {
-    const data = await getData();
+    useEffect(() => {
+        getData(url).then(data => {
+            setData(data);
+            setLoading(false);
+        })
+    }, [url, setData])
+
+    if (loading) return <h3>Loading...</h3>
+
     return (
-        <div className="grid grid-cols-4 gap-6">{data && data.results.map((auction: any) => {
-            <AuctionCard auction={auction} />
-        })}</div>
+        <>
+            <Filters />
+            {data.totalCount === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div className='grid grid-cols-4 gap-6'>
+                        {data.auctions.map(auction => (
+                            <AuctionCard key={auction.id} auction={auction} />
+                        ))}
+                    </div>
+                    <div className='flex justify-center mt-4'>
+                        <AppPagination pageChanged={setPageNumber}
+                            currentPage={params.pageNumber} pageCount={data.pageCount} />
+                    </div>
+                </>
+            )}
+        </>
     )
 }
